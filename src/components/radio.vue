53b2f13f0d4b0663b5abfb6df29e8e363fd63d08
heboliufengjie <heboliufengjie@126.com>
2017-08-04 21:46:44 +0800

    1

--- src/components/radio.vue
+++ src/components/radio.vue
@@ -1,10 +1,12 @@
 <template>
 	<div class="page">
+
 		<mt-header fixed title="电台" class="music-header">
 	      <fallback slot="left"></fallback>
 	    </mt-header>
 		<div class="page-content">
 			<h4 style="text-align: center;">目前无法获取电台Api o(╯□╰)o</h4>
+			<div>hello</div>
 			<h4 style="text-align: center;">
 				欢迎各位在 <a href="https://github.com/Panda-Hope/vue-qqmusic/issues">Issuse</a> 提供建议
 			</h4>
