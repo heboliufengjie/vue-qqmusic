8083b7e1e2ad78c92d85baa5eb682c89e067f09f
fengjie.liu <fengjie.liu@chinascopefinancial.com>
2018-05-30 17:02:51 +0800

    增加本地存储功能点

--- src/components/_login.vue
+++ src/components/_login.vue
@@ -1,217 +1,221 @@
 <style scoped>
- .page{
- 	padding: 0 15px;
- 	background-color: #fff;
- }
- .logo{
- 	width: 115px;
- 	height: 60px;
- 	margin: 0 auto;
- 	background: url('/static/logo.png') no-repeat center;
- 	background-size: cover;
- 	margin-top: calc(30px);
- 	margin-bottom: 16px;
- }
-.mint-checklist-title{
-	margin-left: 0;
-	margin-bottom: 0;
+.page {
+    padding: 0 15px;
+    background-color: #fff;
 }
- .mint-field-core{
- 	border-bottom: 1px solid #ccc;
- 	width: calc(100% );
- 	margin: 8px;
- 	margin-left: 0;
- 	margin-top: 0;
- }
 
- .mint-button--primary {
+.logo {
+    width: 115px;
+    height: 60px;
+    margin: 0 auto;
+    background: url('/static/logo.png') no-repeat center;
+    background-size: cover;
+    margin-top: calc(30px);
+    margin-bottom: 16px;
+}
+
+.mint-checklist-title {
+    margin-left: 0;
+    margin-bottom: 0;
+}
+
+.mint-field-core {
+    border-bottom: 1px solid #ccc;
+    width: calc(100%);
+    margin: 8px;
+    margin-left: 0;
+    margin-top: 0;
+}
+
+.mint-button--primary {
     color: #fff;
     background-color: #26a2ff;
     margin-top: 30px;
 }
-.mint-checklist{
-	position: relative;
-}
-.find-psw{
-	position: absolute;
-	right: 10px;
-	bottom: 16px;
-	font-size: 12px;
-	
-}
-.find-psw a{
-	text-decoration: none;
-	color: #888;
-}
-.other{
-	margin-top: 18px;
-	margin-right: 0;
-}
-.other span{
-	display: inline-block;
-}
-.other .register{
-	float: right;
+
+.mint-checklist {
+    position: relative;
 }
 
-.other a{
-	color: #000;
-	text-decoration: none;
-}
-a.logon_wx{
-	color: #54a037;
-}
-input{
-	user-select:auto; 
+.find-psw {
+    position: absolute;
+    right: 10px;
+    bottom: 16px;
+    font-size: 12px;
 }
 
+.find-psw a {
+    text-decoration: none;
+    color: #888;
+}
+
+.other {
+    margin-top: 18px;
+    margin-right: 0;
+}
+
+.other span {
+    display: inline-block;
+}
+
+.other .register {
+    float: right;
+}
+
+.other a {
+    color: #000;
+    text-decoration: none;
+}
+
+a.logon_wx {
+    color: #54a037;
+}
+
+input {
+    user-select: auto;
+}
 
 </style>
 <template>
-	<div class="page">
-		<p class="logo"></p>
-		<div class="mint-checklist">
-			<label class="mint-checklist-title">邮箱</label>
-			<input type="text"  v-model.trim="email" @blur="blur(email,'email')" class="mint-field-core">
-		</div>
-		<div class="mint-checklist">
-			<label class="mint-checklist-title">密码</label>
-			<input type="password"  v-model.trim="password" @blur="blur(password,'password')"  class="mint-field-core">
-			<label class="find-psw"><a href="/password_find">忘记密码</a></label>
-		</div>
-		<mt-button size="large" class="mint-button--primary" @click="submit()">登录</mt-button>
-		<label class="mint-checklist-title other">
-			<!-- <span><a href="#" class="logon_wx">微信登录</a></span> -->
-			<span class='register'>还没有账号<a href="/register">去注册</a></span>
-		</label>
-	</div>
+    <div class="page">
+        <p class="logo"></p>
+        <div class="mint-checklist">
+            <label class="mint-checklist-title">邮箱</label>
+            <input type="text" v-model.trim="email" @blur="blur(email,'email')" class="mint-field-core">
+        </div>
+        <div class="mint-checklist">
+            <label class="mint-checklist-title">密码</label>
+            <input type="password" v-model.trim="password" @blur="blur(password,'password')" class="mint-field-core">
+            <label class="find-psw"><a href="/password_find">忘记密码</a></label>
+        </div>
+        <mt-button size="large" class="mint-button--primary" @click="submit()">登录</mt-button>
+        <label class="mint-checklist-title other">
+            <!-- <span><a href="#" class="logon_wx">微信登录</a></span> -->
+            <span class='register'>还没有账号<a href="/register">去注册</a></span>
+        </label>
+    </div>
 </template>
 <script>
-	import Vue from 'vue';
-	import { Toast } from 'mint-ui';
-	Vue.http.options.emulateHTTP = true;
-	function isEmail(str){
-	 var reg=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/;
-	 return reg.test(str);
-	}
+import Vue from 'vue';
+import store from '../store';
+import { Toast } from 'mint-ui';
+Vue.http.options.emulateHTTP = true;
 
-	export default {
-		name: 'login',
-		data() {
-			return {
-				email:'',
-				password:'',
-			}
-		},
-		methods:{
-			submit(){
-				if(!this.email){
-					Toast('邮箱不能为空！');
-					return false;
-				}
-				if(!this.password){
-					Toast('密码不能为空！');
-					return false;
-				}
-				
-				this.$http.post("/user/signin.do",{
-					email:this.email,
-					password:this.password,
-				},{
-				  emulateJSON: true
-				}).then(function (res) {
-				// this.$http({
-		  //           url: "/user/signin.do",
-		  //           method: 'POST',
-		  //           // 请求体重发送的数据
-		  //           params: {
-		  //               email:this.email,
-		  //               password:this.password,
-		  //           },
-		  //           // 设置请求头
-		  //           // headers: {
-		  //           //     'Content-Type': 'x-www-from-urlencoded'
-		  //           // }
-		  //       }).then(function () {
-		              if(res.data.success){
-		              	let data = res.data;
+function isEmail(str) {
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/;
+    return reg.test(str);
+}
 
-		              	//存入localStorage
-		              	if(!window.localStorage){
-				            alert("浏览器支持localstorage");
-				            return false;
-				        }else{
-				            var storage=window.localStorage;
-				            var d=JSON.stringify(data);
-				            storage.setItem("teamUp",d);
-				             //将JSON字符串转换成为JSON对象输出
-				            var json=storage.getItem("teamUp");
-				            var jsonObj=JSON.parse(json);
-				            //删除
-				            //storage.removeItem("teamUp");
-				        }
-		              
-		              	//登录成功后跳转到首页列表
-		              	location.href="/lists"
-		              }else{
-		              	Toast(res.data.msg)
-		              }
-		            }
-		        );
-		       
-		        //获得一个标签下的项目 debug
-				if(false){
-					this.$http.post("/project/getProjectInLabel.do",{
-						labelId:5//debug
-					},{
-					  emulateJSON: true
-					}
-					).then(function (res) {
-			              if(res.data.success){
-			              	console.log('创建项目')
-			              	//Toast('修改成功')
-			              }else{
-			              	Toast(res.data.msg)
-			              }
-			            }
-			        );
-		        }
+export default {
+    name: 'login',
+    data() {
+        return {
+            email: '',
+            password: '',
+        }
+    },
+    // created() {
+    //     let storage = window.localStorage;
+    //     let storageItem = storage.getItem("teamUp");
+    //     // console.log('created', storageItem)
+    //     if (storageItem && storageItem.id) {
+    //         //localStorage 已经存有数据
+    //         location.href = '/lists';
+    //     } else {
+    //         //localStorage 没有存数据
+    //         //console.log('storageItem02', storageItem)
+    //     }
+    // },
+    methods: {
+        submit() {
+            if (!this.email) {
+                Toast('邮箱不能为空！');
+                return false;
+            }
+            if (!this.password) {
+                Toast('密码不能为空！');
+                return false;
+            }
 
-		       
-		         //获得标签信息
-				if(false){
-					this.$http.post("/label/getLableInfo.do",{
-						id:5,
-					},{
-					  emulateJSON: true
-					}
-					).then(function (res) {
-			              if(res.data.success){
-			              	console.log('创建项目')
-			              	//Toast('修改成功')
-			              }else{
-			              	Toast(res.data.msg)
-			              }
-			            }
-			        );
-		        }
-		       
-			},
-			blur(data,type){
-				if(type ==='email'){
-					if(!data){
-						Toast(' 邮箱不能为空！');
-					}else if(!isEmail(data)){
-						Toast(' 请重新填写邮箱！');
-						return false;
-					}
-				}else if(type ==='password'){
-					if(!data){
-						Toast(' 密码不能为空！');
-					}
-				}
-			}
-		},
-	};
+            this.$http.post("/user/signin.do", {
+                email: this.email,
+                password: this.password,
+            }, {
+                emulateJSON: true
+            }).then(function(res) {
+                // this.$http({
+                //           url: "/user/signin.do",
+                //           method: 'POST',
+                //           // 请求体重发送的数据
+                //           params: {
+                //               email:this.email,
+                //               password:this.password,
+                //           },
+                //           // 设置请求头
+                //           // headers: {
+                //           //     'Content-Type': 'x-www-from-urlencoded'
+                //           // }
+                //       }).then(function () {
+                if (res.data.success) {
+                    let data = res.data;
+
+                    //存入localStorage
+                    store.dispatch('localStorage/setItem', data)
+                    this.$router.push("lists")
+                } else {
+                    Toast(res.data.msg)
+                }
+            });
+
+            //获得一个标签下的项目 debug
+            if (false) {
+                this.$http.post("/project/getProjectInLabel.do", {
+                    labelId: 5 //debug
+                }, {
+                    emulateJSON: true
+                }).then(function(res) {
+                    if (res.data.success) {
+                        console.log('创建项目')
+                        //Toast('修改成功')
+                    } else {
+                        Toast(res.data.msg)
+                    }
+                });
+            }
+
+
+            //获得标签信息
+            if (false) {
+                this.$http.post("/label/getLableInfo.do", {
+                    id: 5,
+                }, {
+                    emulateJSON: true
+                }).then(function(res) {
+                    if (res.data.success) {
+                        console.log('创建项目')
+                        //Toast('修改成功')
+                    } else {
+                        Toast(res.data.msg)
+                    }
+                });
+            }
+
+        },
+        blur(data, type) {
+            if (type === 'email') {
+                if (!data) {
+                    Toast(' 邮箱不能为空！');
+                } else if (!isEmail(data)) {
+                    Toast(' 请重新填写邮箱！');
+                    return false;
+                }
+            } else if (type === 'password') {
+                if (!data) {
+                    Toast(' 密码不能为空！');
+                }
+            }
+        }
+    },
+};
+
 </script>
-
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import store from '../store'
 import { Indicator } from 'mint-ui'
+//import { mapState, mapGetters } from 'vuex';
+
 
 Vue.use(Router)
 
@@ -9,8 +11,8 @@ const router = new Router({
     linkActiveClass: '',
     mode: 'history',
     routes: [{
-            path: '/',
-            name: 'index',
+            path: '/login',
+            name: 'login',
             component(resolve) {
                 require(['@/components/_login'], resolve);
             }
@@ -20,14 +22,20 @@ const router = new Router({
             name: 'register',
             component(resolve) {
                 require(['@/components/_register'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '/password_find',
             name: 'password_find',
             component(resolve) {
                 require(['@/components/_password_find'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         //getSystemRecommendUser
         // {
@@ -42,14 +50,20 @@ const router = new Router({
             name: 'me',
             component(resolve) {
                 require(['@/components/_me'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '/personal_display/:id',
             name: 'personal_display',
             component(resolve) {
                 require(['@/components/_personal_display'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         // {
         //     path: '/lables_edit/:id',
@@ -63,44 +77,78 @@ const router = new Router({
             name: 'lists',
             component(resolve) {
                 require(['@/components/_lists'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '/item_add',
             name: 'item_add',
             component(resolve) {
                 require(['@/components/_item_add'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '/item_detail/:id',
             name: 'item_detail',
             component(resolve) {
                 require(['@/components/_item_detail'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '/item_edit/:id',
             name: 'item_edit',
             component(resolve) {
                 require(['@/components/_item_edit'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
         {
             path: '*',
             name: '*',
             component(resolve) {
                 require(['@/components/_lists'], resolve);
-            }
+            },
+            meta: {
+                requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
+            },
         },
     ]
 });
 
-// router.beforeEach((to, from, next) => {
-//   // Indicator.open('加载中...');
-//   console.log(11)
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+    // Indicator.open('加载中...');
+    let info = store.state.localStorage.info;
+    if (to.meta.requireAuth) {
+        // 
+        //setTimeout(() => {
+        //  console.log('1111>>>', info, (info && info.email))
+        //}, 0);
+
+        if (info && info.email) {
+            next();
+        } else {
+            next('/login')
+        }
+    } else {
+        // console.log('1111>>>', info, (info && info.email))
+        // console.log('>>', to)
+        if ((info && info.email) && to.name == 'login') {
+            next('/lists')
+        } else {
+            next()
+        }
+    }
+});
 
 // router.afterEach((to) => {
 //   store.commit('blurredPage/switchBlurredPage', to.name);
